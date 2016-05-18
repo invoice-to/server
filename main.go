ce2160e2870997f4f53e57dd6375f16542a62009
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-18 13:56:41 -0700

    add twitter keys

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/ChimeraCoder/anaconda"
+	config "./config"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/auth", authHandler)
+	setupTwitter(config.TwitterTokens.key, config.TwitterTokens.secret)
+}
+
+func setupTwitter(key string, secret string) {
+	fmt.Printf("setting up")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	fmt.Fprint(w, "👋 \ncheck out /auth (it's 🔥)")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "yo")
 }
